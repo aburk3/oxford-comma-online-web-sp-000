8cd5b574106f622630d3ddb7f0189348e0a18bce
Austin  Burke <aburk3@gmail.com>
2018-09-29 21:54:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size > 1 
     last_person = array.pop
     my_people = array.join(", ")
-    my_people << " and #{last_person}"
+    my_people << ", and #{last_person}"
   else
     return array.join
   end
